a3d5abe1f07c2b65b72dbe940755b4ca095f08be
AiduKnow <94282596+Aiduknow@users.noreply.github.com>
2023-01-31 08:41:29 +0100

    Update JasPooApplication.java

--- src/main/java/MavenTP3/jas_poo/JasPooApplication.java
+++ src/main/java/MavenTP3/jas_poo/JasPooApplication.java
@@ -2,6 +2,11 @@ package MavenTP3.jas_poo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import fr.ensim.info.tp3aymaneouhadi.model.Address;
+import fr.ensim.info.tp3aymaneouhadi.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 
 @SpringBootApplication
 public class JasPooApplication {
